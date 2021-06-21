ef80232f00b70e4a2401d2a7ecdb1eb36925e4dd
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-21 12:23:40 -0600

    initial commit without accessing account balance

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    class BankAccount
+   {
+        public BankAccount()
+        {
+            Balance = 0;
+        }
+        private static double Balance { get; set; }
+
+        public static void  Deposit(double deposit)
+        {
+            BankAccount.Balance = deposit + BankAccount.Balance;
+        }
+        public static double GetBalance()
+        {
+           
+            return BankAccount.Balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount account1 = new BankAccount();
+            
+            do
+            {
+
+                Console.WriteLine("Would you like to deposit money or retrieve your balance?");
+                if (Console.ReadLine() == "deposit money")
+                {
+                    Console.WriteLine("How much would you like to deposit?");
+                    BankAccount.Deposit(double.Parse(Console.ReadLine()));
+                    Console.WriteLine("Would you like to see your new account balance? (y/n)");
+                    if (Console.ReadLine() == "y")
+                    {
+                        Console.WriteLine(BankAccount.GetBalance());
+                       
+                    }
+                    else if (Console.ReadLine()== "exit")
+                    {
+                        break;
+                    }
+                   
+                }
+                else if (Console.ReadLine() == "retrieve balance")
+                {
+                    Console.WriteLine(BankAccount.GetBalance());
+                    
+                }
+                else if (Console.ReadLine() == "exit")
+                {
+                    break;
+                }
+            } while ( true);
+        }
+    }
+}
